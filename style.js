08259ca81bcccd80f0c08c9b859a636597623e0e
Elena <kolomiets.exceedteam@gmail.com>
2021-12-07 17:31:10 +0300

    ГОТОВО!

--- style.js
+++ style.js
@@ -1,11 +1,68 @@
 
 
 
+/*$('.TodoItemClone').parentsUntil().siblings('.item').css('background', '#ef6e69');*/
 
-$("#click").click(function () {
 
 
-	  	  
+$('.c1').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#ef6e69');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#ef6e69');
+
+
+});
+
+
+$('.c2').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#f279a2');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#f279a2');
+
+
+});
+
+$('.c3').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#9170cb');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#9170cb');
+
+
+});
+$('.c4').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#5eb3f6');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#5eb3f6');
+
+
+});
+$('.c5').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#67d7e5');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#67d7e5');
+
+
+});
+$('.c6').click(function(){
+
+$('.TodoItemClone:checked').parents('.container_input').siblings('.item').css('background', '#fee183');
+
+$('#TodoItem:checked').parents('.container_input').siblings('.item').css('background', '#fee183');
+
+
+});
+
+
+
+
+
+
+$("#click").click(function () {	  	  
 $(".div").addClass('create').last().clone().appendTo('.wrapper_div');
 
 $('.div').prev().removeClass('create');
@@ -22,29 +79,14 @@ $(".create").find('.span_text_todo').css('display','none');
 
 $(".create").find('.Todo').addClass('TodoText');
 $(".create").find('.item').css('background',colors[Math.floor(Math.random() * colors.length)]);
+$(".create").find('#TodoItem').addClass('TodoItemClone');
+
+$('.div').attr('id', function() { return $('.div').index()+"1"; });
 
 });
 
-function(){     
-    if($(".c1").attr("checked") == 'checked') { 
-        
-        $('.item').css('background','color(#ef6e69)');
-        
-        return true;
-    }
-    return false;
-};
 
 
-
-/*
-$('.c1').click(function(){
-$("#agree").prop('checked')	
-
-
-
-});
-*/
 
 $('#click').click(function(){
 	
@@ -53,3 +95,15 @@ var val = $('#AddNewItem').val();//Получаем данные из input
 
 });
 
+
+/*$('.c1').css('background', '#ef6e69');*/
+
+
+
+
+
+
+
+
+
+            
