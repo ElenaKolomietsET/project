de5434efc7983114692a6cf9f6ac7efd61cf4751
Elena <kolomiets.exceedteam@gmail.com>
2021-12-07 13:58:27 +0300

    почти

--- style.js
+++ style.js
@@ -1,34 +1,46 @@
 
 
 
+
+$("#click").click(function () {
+
+
+	  	  
+$(".div").addClass('create').last().clone().appendTo('.wrapper_div');
+
+$('.div').prev().removeClass('create');
+
+$('.Todo').removeClass('TodoText');
+
+    });
+
 $('#click').click(function(){
-	
-$(".div").find('#Todo').removeClass('create');
+	      let colors = ['#ef6e69','#f279a2','#9170cb','#5eb3f6','#5eb3f6','#fee183'];
+
+
+$(".create").find('.span_text_todo').css('display','none');
+
+$(".create").find('.Todo').addClass('TodoText');
+$(".div").find('.item').css('background','colors[ Math.floor(Math.random() * colors.length) ]');
 
 });
 
 
-$("#click").click(function () {
-
-      let colors = ['#ef6e69','#f279a2','#9170cb','#5eb3f6','#5eb3f6','#fee183'];
-	  	  
-/*$(".div").last().clone().appendTo('.wrapper_div').find('div.item').css('background',colors[Math.floor(Math.random() * colors.length)]);*/
 
 
 
-$(".div").last().clone().appendTo('.wrapper_div').find('div.item').css('background',colors[Math.floor(Math.random() * colors.length)]).find('#Todo').addClass('create');
+/*
+$('#click').click(function(){
+	
+$(".create").find('.Todo').removeClass('TodoItem');
 
-$(".div").last().clone().appendTo('.wrapper_div').find('#Todo').addClass('create');
-
-$(".div").last().clone().appendTo('.wrapper_div').find(".span_text_todo").css('display','none');
+});*/
 
 
-
-    });
 $('#click').click(function(){
 	
 var val = $('#AddNewItem').val();//Получаем данные из input
-  $('.create').html(val);//Вставляем значение в тег с классом txt
+  $('.TodoText').html(val);//Вставляем значение в тег с классом txt
 
 });
 
